e27e49b88a46c33a78d9411ec488d9b32d2ea8d0
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2021-01-14 17:57:15 +0100

    fixed Z.{forall,exists}

--- src/runtime/gospel_runtime.ml
+++ src/runtime/gospel_runtime.ml
@@ -79,10 +79,10 @@ module Z = struct
   include Z
 
   let rec forall start stop p =
-    if equal start stop then p start else p start && forall (succ start) stop p
+    start > stop || (p start && forall (succ start) stop p)
 
   let rec exists start stop p =
-    if equal start stop then p start else p start || exists (succ start) stop p
+    start <= stop && (p start || exists (succ start) stop p)
 end
 
 module Array = struct
