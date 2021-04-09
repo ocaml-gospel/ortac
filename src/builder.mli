37f1d66cda839c1f4bfd10fc2316fe6f8ccbb79a
Nicolas Osborne <nicolas.osborne.etu@univ-lille.fr>
2021-04-09 14:23:32 +0200

    move building blocks in builder

--- src/builder.mli
+++ src/builder.mli
@@ -1,4 +1,5 @@
 open Ppxlib
+open Gospel
 
 include Ast_builder.S
 
@@ -46,3 +47,27 @@ val failed_xpost_nonexec :
   expression
 
 val efun : (arg_label * pattern) list -> expression -> expression
+
+exception Unsupported of Location.t option * string
+
+val lident : label -> longident loc
+
+val post : label -> label -> expression -> Tterm.term list -> expression
+
+val pre : label -> label -> expression -> Tterm.term list -> expression
+
+val xpost_guard :
+  label ->
+  'a ->
+  label ->
+  expression ->
+  (Ttypes.xsymbol * (Tterm.pattern * Tterm.term) list) list ->
+  expression ->
+  expression
+
+val of_gospel_args :
+  Tast.lb_arg list -> (arg_label * expression) list * (arg_label * pattern) list
+
+val returned_pattern : Tast.lb_arg list -> pattern * expression
+
+val mk_open : structure_item
