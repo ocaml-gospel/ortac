cf47302de59bc5cfb8faff9ce455e73f7353d5c6
Nicolas Osborne <nicolas.osborne.etu@univ-lille.fr>
2021-04-28 13:59:08 +0200

    generators in module and array param

--- src/monolith/backend.ml
+++ src/monolith/backend.ml
@@ -26,22 +26,78 @@ let mk_candidate module_name =
   in
   A.pstr_module ~loc module_bind
 
-let is_arrow = function Ptyp_arrow _ -> true | _ -> false
+module Generators = struct
+  let rec string2gen s params =
+    match s with
+    | "unit" -> [%expr Gen.unit]
+    | "bool" -> [%expr Gen.bool]
+    | "char" -> [%expr Gen.char]
+    | "int" -> [%expr Gen.int 1024]
+    | "string" -> [%expr Gen.string (Gen.int 1024) Gen.char]
+    | "array" -> (
+        match params with
+        | [] -> failwith "array should have a parameter"
+        | [ param ] -> [%expr Gen.array (Gen.int Int.max_int) [%e ty2gen param]]
+        | _ -> failwith "don't know what to do with more than one parameter")
+    | s -> failwith (Printf.sprintf "%s is not yet implemented" s)
 
-let gen ty params =
-  match ty with
-  | "unit" -> [%expr Gen.unit]
-  | "bool" -> [%expr Gen.bool]
-  | "char" -> [%expr Gen.char]
-  | "int" -> [%expr Gen.int 1024]
-  | "string" -> [%expr Gen.string (Gen.int 1024) Gen.char]
-  | "array" -> (
-      match params with
-      | [] -> failwith "array should have a parameter"
-      | [ _param ] ->
-          [%expr Gen.array (Gen.int Int.max_int) Gen.char] (* FIXME *)
-      | _ -> failwith "don't know what to do with more than one parameter")
-  | s -> failwith (Printf.sprintf "%s is not yet implemented" s)
+  and ty2gen (ty : Gospel.Ttypes.ty) =
+    match ty.ty_node with
+    | Tyvar tvs -> string2gen tvs.tv_name.id_str []
+    | Tyapp (tys, params) -> string2gen tys.ts_ident.id_str params
+
+  let generator_nr _ = failwith "not yet implemented"
+
+  let record_gen ty_name (rec_decl : Gospel.Tast.rec_declaration) =
+    let id = B.pvar ty_name in
+    let get_field (ld : Gospel.Tterm.lsymbol Gospel.Tast.label_declaration) =
+      ld.ld_field.ls_name.id_str
+    in
+    let get_ty (ld : Gospel.Tterm.lsymbol Gospel.Tast.label_declaration) =
+      match ld.ld_field.ls_value with
+      | Some ty -> [%expr [%e ty2gen ty] ()]
+      | None -> failwith "can't find type"
+    in
+    let r =
+      A.pexp_record ~loc
+        (List.map (fun x -> (B.lident (get_field x), get_ty x)) rec_decl.rd_ldl)
+        None
+    in
+    [%stri let [%p id] = fun () -> [%e r]]
+
+  let generator_r (type_decl : Gospel.Tast.type_declaration) =
+    let ty_name = type_decl.td_ts.ts_ident.id_str in
+    match type_decl.td_kind with
+    | Gospel.Tast.Pty_record rec_decl -> record_gen ty_name rec_decl
+    | _ -> failwith "not yet implemented"
+
+  let generator rec_flag type_decl =
+    match rec_flag with
+    | Gospel.Tast.Nonrecursive -> generator_nr type_decl
+    | Gospel.Tast.Recursive -> generator_r type_decl
+
+  let mk_generator (sig_item : Gospel.Tast.signature_item) =
+    match sig_item.sig_desc with
+    | Gospel.Tast.Sig_type (rec_flag, [ type_decl ], _) ->
+        Some (generator rec_flag type_decl)
+    | _ -> None
+
+  let mk_generators s =
+    let gen = List.filter_map mk_generator s in
+    let module_g = A.pmod_structure ~loc gen in
+    let module_bind =
+      A.module_binding ~loc ~name:(B.noloc (Some "G")) ~expr:module_g
+    in
+    A.pstr_module ~loc module_bind
+end
+
+module Printers = struct end
+
+open Generators
+
+module Specs = struct end
+
+let is_arrow = function Ptyp_arrow _ -> true | _ -> false
 
 let find_gen s =
   match s.ptyp_desc with
@@ -102,53 +158,6 @@ and translate_constr ty params =
       | _ -> failwith "don't know what to do with Array with multiple params")
   | t -> failwith (Printf.sprintf "%s is not yet implemented" t)
 
-let generator_nr _ = failwith "not yet implemented"
-
-let record_gen ty_name (rec_decl : Gospel.Tast.rec_declaration) =
-  let id = B.pvar ty_name in
-  let get_field (ld : Gospel.Tterm.lsymbol Gospel.Tast.label_declaration) =
-    ld.ld_field.ls_name.id_str
-  in
-  let get_ty (ld : Gospel.Tterm.lsymbol Gospel.Tast.label_declaration) =
-    match ld.ld_field.ls_value with
-    | Some ty -> (
-        match ty.ty_node with
-        | Tyvar tvs -> [%expr [%e gen tvs.tv_name.id_str []] ()]
-        | Tyapp (tys, _) -> [%expr [%e gen tys.ts_ident.id_str []] ()])
-    | None -> failwith "can't find type"
-  in
-  let r =
-    A.pexp_record ~loc
-      (List.map (fun x -> (B.lident (get_field x), get_ty x)) rec_decl.rd_ldl)
-      None
-  in
-  [%stri let [%p id] = fun () -> [%e r]]
-
-let generator_r (type_decl : Gospel.Tast.type_declaration) =
-  let ty_name = type_decl.td_ts.ts_ident.id_str in
-  match type_decl.td_kind with
-  | Gospel.Tast.Pty_record rec_decl -> record_gen ty_name rec_decl
-  | _ -> failwith "not yet implemented"
-
-let generator rec_flag type_decl =
-  match rec_flag with
-  | Gospel.Tast.Nonrecursive -> generator_nr type_decl
-  | Gospel.Tast.Recursive -> generator_r type_decl
-
-let mk_generator (sig_item : Gospel.Tast.signature_item) =
-  match sig_item.sig_desc with
-  | Gospel.Tast.Sig_type (rec_flag, [ type_decl ], _) ->
-      Some (generator rec_flag type_decl)
-  | _ -> None
-
-let mk_generators s =
-  let gen = List.filter_map mk_generator s in
-  let module_g = A.pmod_structure ~loc gen in
-  let module_bind =
-    A.module_binding ~loc ~name:(B.noloc (Some "G")) ~expr:module_g
-  in
-  A.pstr_module ~loc module_bind
-
 let mk_declaration (sig_item : Gospel.Tast.signature_item) =
   match sig_item.sig_desc with
   | Gospel.Tast.Sig_val (decl, _ghost) ->
@@ -184,7 +193,8 @@ let standalone module_name s =
   let mod_gen = mk_generators s in
   let specs = mk_specs s in
   [%stri open Monolith]
-  :: [%stri module M = Monolith_runtime] :: mod_ref :: mod_can :: specs
+  ::
+  [%stri module M = Monolith_runtime] :: mod_ref :: mod_can :: mod_gen :: specs
 
 let generate path =
   let module_name = Ortac_core.Utils.module_name_of_path path in
