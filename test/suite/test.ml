96e4766b4e53bb19a14e6873672263194e9ed6dd
Clément Pascutto <clement@pascutto.fr>
2021-05-03 15:06:33 +0200

    Upgrade to the latest gospel version

--- test/suite/test.ml
+++ test/suite/test.ml
@@ -1,4 +1,4 @@
 let () =
   Fmt.(set_style_renderer stderr `Ansi_tty);
   Alcotest.run "Gospel-rtac"
-    [ Arrays.suite; Arith.suite; Exceptions.suite; Terms.suite ]
+    [ Arrays.suite; Arith.suite; Exceptions.suite; Terms.suite; Types.suite ]
---
+++ test/suite/types/lib.ml
@@ -0,0 +1,3 @@
+type t = { x : int; y : bool }
+
+let get_x t = t.x
