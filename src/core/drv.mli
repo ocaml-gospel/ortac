514527f7d011f7064abc03a9b69c0edf35e52c60
Clément Pascutto <clement@pascutto.fr>
2021-09-23 10:32:44 +0200

    drv: add get_ts

--- src/core/drv.mli
+++ src/core/drv.mli
@@ -5,3 +5,5 @@ val v : Gospel.Tmodule.namespace list -> t
 val translate : t -> Gospel.Tterm.lsymbol -> string option
 
 val get_ls : t -> string list -> Gospel.Tterm.lsymbol
+
+val get_ts : t -> string list -> Gospel.Ttypes.tysymbol
