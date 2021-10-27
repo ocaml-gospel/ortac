c44b414c143c0a1cf53c801f7f165a869cd25e28
n-osborne <nicolas.osborne@tarides.com>
2021-10-27 11:52:09 +0200

    deduplicate code

--- src/default/ortac_default.ml
+++ src/default/ortac_default.ml
@@ -4,20 +4,16 @@ end
 
 module G = Ortac_core.Ortac.Make (M)
 
-let generate path output =
+let run f path =
   let module_name = Ortac_core.Utils.module_name_of_path path in
-  let output = Format.formatter_of_out_channel output in
   Gospel.Parser_frontend.parse_ocaml_gospel path
   |> Ortac_core.Utils.type_check [] path
   |> fun (env, sigs) ->
   assert (List.length env = 1);
-  G.signature module_name (List.hd env) sigs
-  |> Fmt.pf output "%a@." Ppxlib_ast.Pprintast.structure
+  f module_name (List.hd env) sigs
 
-let report path =
-  let module_name = Ortac_core.Utils.module_name_of_path path in
-  Gospel.Parser_frontend.parse_ocaml_gospel path
-  |> Ortac_core.Utils.type_check [] path
-  |> fun (env, sigs) ->
-  assert (List.length env = 1);
-  G.report module_name (List.hd env) sigs
+let generate path output =
+  let output = Format.formatter_of_out_channel output in
+  run G.signature path |> Fmt.pf output "%a@." Ppxlib_ast.Pprintast.structure
+
+let report = run G.report
