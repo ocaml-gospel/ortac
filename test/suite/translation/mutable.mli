a03c28ea5f8e65be6aa42142ea06141f0782d928
n-osborne <nicolas.osborne@tarides.com>
2021-11-02 16:37:41 +0100

    add test for pure

--- test/suite/translation/mutable.mli
+++ test/suite/translation/mutable.mli
@@ -4,10 +4,11 @@ type t_ephemeral
 type 'a with_mutable_model
 (*@ mutable model content : 'a set *)
 
-type record_with_mutable_field = { mutable m : int }
-type variant_with_known_mutable_field = K of t_ephemeral
+type record_with_mutable_flag = { mutable m : int }
 type record_with_known_mutable_field = { t : t_ephemeral }
+type variant_with_known_mutable_field = K of t_ephemeral
 type int_array = int array
 type int_ref = int ref
+type list_of_mutable = int ref list
 type variant = C of int array
 type record = { f : int array }
