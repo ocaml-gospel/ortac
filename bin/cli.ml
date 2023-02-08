a1af6eb83410cb5f242db7d1845c2fc705965afe
n-osborne <nicolas.osborne@tarides.com>
2023-02-08 10:48:41 +0100

    Changes frontends into subcommands
    The perspective of this change is to have plugins bring new subcommands.
    Also, the futur stm mode will need different options than the existing
    modes.

--- bin/cli.ml
+++ bin/cli.ml
@@ -1,25 +1,4 @@
-type frontend = Default | Monolith
-
 let get_channel = function None -> stdout | Some path -> open_out path
-
-let frontend_printer ppf = function
-  | Default -> Fmt.string ppf "Default"
-  | Monolith -> Fmt.string ppf "Monolith"
-
-let frontend_parser = function
-  | "default" -> Ok Default
-  | "monolith" -> Ok Monolith
-  | s -> Error (`Msg (Fmt.str "Error: `%s' is not a valid argument" s))
-
-let main frontend input output () =
-  let channel = get_channel output in
-  try
-    match frontend with
-    | Default -> Ortac_default.generate input channel
-    | Monolith -> Ortac_monolith.generate input channel
-  with Gospel.Warnings.Error e ->
-    Fmt.epr "%a@." Gospel.Warnings.pp e;
-    exit 1
 
 open Cmdliner
 
@@ -36,7 +15,10 @@ let output_file =
   Arg.(
     value
     & opt (conv ~docv:"OUTPUT" (parse, Fmt.(option string))) None
-    & info [ "o"; "output" ] ~docv:"OUTPUT")
+    & info [ "o"; "output" ] ~absent:"stdout" ~docv:"OUTPUT"
+        ~doc:
+          "Prints the generated code in OUTPUT. OUTPUT shouldn't be the name \
+           of a directory. Creates the file if it does not exists.")
 
 let ocaml_file =
   let parse s =
@@ -49,19 +31,43 @@ let ocaml_file =
   in
   Arg.(required & pos 0 (some (parse, Fmt.string)) None & info [] ~docv:"FILE")
 
-let frontend =
-  Arg.(
-    value
-    & opt (conv ~docv:"FRONTEND" (frontend_parser, frontend_printer)) Default
-    & info [ "f"; "frontend" ] ~docv:"FRONTEND")
+module Default : sig
+  val cmd : unit Cmd.t
+end = struct
+  let main input output () =
+    let channel = get_channel output in
+    try Ortac_default.generate input channel
+    with Gospel.Warnings.Error e ->
+      Fmt.epr "%a@." Gospel.Warnings.pp e;
+      exit 1
 
-let cmd =
+  let info = Cmd.info "default" ~doc:"Simple assertion checking wrapper."
+  let term = Term.(const main $ ocaml_file $ output_file $ setup_log)
+  let cmd = Cmd.v info term
+end
+
+module Monolith : sig
+  val cmd : unit Cmd.t
+end = struct
+  let main input output () =
+    let channel = get_channel output in
+    try Ortac_monolith.generate input channel
+    with Gospel.Warnings.Error e ->
+      Fmt.epr "%a@." Gospel.Warnings.pp e;
+      exit 1
+
+  let info =
+    Cmd.info "monolith"
+      ~doc:"Generates Monolith test file according to Gospel specifications."
+
+  let term = Term.(const main $ ocaml_file $ output_file $ setup_log)
+  let cmd = Cmd.v info term
+end
+
+let group =
   let doc = "Run ORTAC." in
   let version = "ortac version %%VERSION%%" in
   let info = Cmd.info "ortac" ~doc ~version in
-  let term =
-    Term.(const main $ frontend $ ocaml_file $ output_file $ setup_log)
-  in
-  Cmd.v info term
+  Cmd.group info [ Default.cmd; Monolith.cmd ]
 
-let () = Stdlib.exit (Cmd.eval cmd)
+let () = Stdlib.exit (Cmd.eval group)
