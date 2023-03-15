f84f73faa8073bbe91a5846047eedc3bb59dfd14
n-osborne <nicolas.osborne@tarides.com>
2023-03-15 15:37:22 +0100

    Add a config and init function for it
    The config will hold the information necessary to build the qcheck-stm
    test and that are given by the user such as the type of the system under
    test and the init function.
    
    The Config.init function check that there is actually a type for the
    given sut in the given module. It also initialize the translation
    context from Ortac_core.

--- plugins/qcheck-stm/src/ortac_qcheck_stm.ml
+++ plugins/qcheck-stm/src/ortac_qcheck_stm.ml
@@ -1,4 +1,14 @@
-let main () = print_endline "Hello"
+let main path init sut =
+  match Config.init path init sut with
+  | Ok config ->
+      let open Gospel.Identifier.Ident in
+      let msg =
+        "This will generate qcheck-stm tests using `"
+        ^ config.sut.ts_ident.id_str
+        ^ "' as principal type"
+      in
+      print_endline msg
+  | Error err -> print_endline err
 
 open Cmdliner
 
@@ -9,7 +19,26 @@ end = struct
     Cmd.info "qcheck-stm"
       ~doc:"Generate QCheck-stm test file according to Gospel specifications."
 
-  let term = Term.(const main $ const ())
+  let sut =
+    Arg.(
+      required
+      & pos 1 (some string) None
+      & info [] ~doc:"Build the qcheck-stm tests with SUT." ~docv:"SUT")
+
+  let init =
+    Arg.(
+      required
+      & pos 2 (some string) None
+      & info []
+          ~doc:
+            "Build the qcheck-stm tests using INIT function to initialize the \
+             system under test."
+          ~docv:"INIT")
+
+  let term =
+    let open Registration in
+    Term.(const main $ ocaml_file $ init $ sut)
+
   let cmd = Cmd.v info term
 end
 
