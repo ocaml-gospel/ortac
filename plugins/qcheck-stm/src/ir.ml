f096f93c481dd7c8acff34b67784ed0d6bef68ac
n-osborne <nicolas.osborne@tarides.com>
2023-03-24 16:54:01 +0100

    Define the intermediate representation for the stm plugin
    The intermediate representation collects all the information from gospel
    needed for generating the stm module.
    
    Co-authored-by: Samuel Hym <samuel.hym@rustyne.lautre.net>

---
+++ plugins/qcheck-stm/src/ir.ml
@@ -0,0 +1,28 @@
+open Gospel
+module Ident = Identifier.Ident
+
+type xpost = Ttypes.xsymbol * (Tterm.pattern * Tterm.term) list
+
+type next_state = {
+  formulae : Tterm.term list;
+  modifies : Tterm.term list;
+  checks : Tterm.term list;
+}
+
+type postcond = {
+  normal : Tterm.term list;
+  exceptional : xpost list;
+  checks : Tterm.term list;
+}
+
+type value = {
+  id : Ident.t;
+  ty : Ppxlib.core_type;
+  sut_var : Ident.t;
+  args : Ident.t list;
+  next_state : next_state;
+  postcond : postcond;
+  precond : Tterm.term list;
+}
+
+type t = value list
