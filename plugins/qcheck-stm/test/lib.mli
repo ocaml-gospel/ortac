ea95187428b81652c118274b308281b290f6889b
n-osborne <nicolas.osborne@tarides.com>
2023-03-28 10:01:35 +0200

    Test the filtering of values
    Complete the tested lib and print:
    - all the errors when there is
    - the name of the value when it passes the filtering.

--- plugins/qcheck-stm/test/lib.mli
+++ plugins/qcheck-stm/test/lib.mli
@@ -2,3 +2,8 @@ type sut
 
 val good_init : unit -> sut
 val bad_init : sut
+val no_sut : int -> int
+val constant : int
+val multiple_sut : sut -> sut -> int
+val f : sut -> int
+val g : int -> sut -> bool
