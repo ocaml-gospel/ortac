076f231fd604188b2d2112ed94265d97a95eab23
n-osborne <nicolas.osborne@tarides.com>
2023-03-29 16:40:46 +0200

    Make the reserr monad use the extensible Ortac_core warning mechanism
    Change the type Reserr.error to be an extension of Warnings.kind, define
    necessary formatters and use them in the test.

--- plugins/qcheck-stm/test/expect_ir.ml
+++ plugins/qcheck-stm/test/expect_ir.ml
@@ -1,4 +1,5 @@
 open Ortac_qcheck_stm
 
-let pp r = Reserr.to_string Ir.Pp.value r |> print_endline
-let () = Ir_of_gospel.run "lib.mli" "good_init" "sut" |> List.iter pp
+let () =
+  let pp = Fmt.(list (Reserr.pp Ir.pp_value) stdout) in
+  Ir_of_gospel.run "lib.mli" "good_init" "sut" |> pp
