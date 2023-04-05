f502931ea1930c067b4e72dc1a0ca0ab7b8c3e06
n-osborne <nicolas.osborne@tarides.com>
2023-04-05 10:57:27 +0200

    Rename qcheck-stm warnings to be more uniform with core

--- plugins/qcheck-stm/src/reserr.ml
+++ plugins/qcheck-stm/src/reserr.ml
@@ -1,17 +1,17 @@
 module W = Ortac_core.Warnings
 
 type W.kind +=
-  | Value_is_a_constant of string
-  | Value_return_sut of string
-  | Value_have_no_sut_argument of string
-  | Value_have_multiple_sut_arguments of string
+  | Constant_value of string
+  | Returning_sut of string
+  | No_sut_argument of string
+  | Multiple_sut_arguments of string
 
 let level kind =
-    match kind with
-    | Value_is_a_constant _ | Value_return_sut _ | Value_have_no_sut_argument _
-    | Value_have_multiple_sut_arguments _ ->
-        W.Warning
-    | _ -> W.level kind
+  match kind with
+  | Constant_value _ | Returning_sut _ | No_sut_argument _
+  | Multiple_sut_arguments _ ->
+      W.Warning
+  | _ -> W.level kind
 
 type 'a reserr = ('a, W.t list) result
 
@@ -28,14 +28,13 @@ let ( and* ) a b =
 open Fmt
 
 let pp_kind ppf kind =
-    match kind with
-    | Value_is_a_constant id -> pf ppf "%a is a constant." W.quoted id
-    | Value_return_sut id -> pf ppf "%a returns a sut." W.quoted id
-    | Value_have_no_sut_argument id ->
-        pf ppf "%a have no sut argument." W.quoted id
-    | Value_have_multiple_sut_arguments id ->
-        pf ppf "%a have multiple sut arguments." W.quoted id
-    | _ -> W.pp_kind ppf kind
+  match kind with
+  | Constant_value id -> pf ppf "%a is a constant." W.quoted id
+  | Returning_sut id -> pf ppf "%a returns a sut." W.quoted id
+  | No_sut_argument id -> pf ppf "%a have no sut argument." W.quoted id
+  | Multiple_sut_arguments id ->
+      pf ppf "%a have multiple sut arguments." W.quoted id
+  | _ -> W.pp_kind ppf kind
 
 let pp_errors = W.pp_param pp_kind level |> list
 let pp pp_ok = Fmt.result ~ok:pp_ok ~error:pp_errors
--- plugins/qcheck-stm/src/reserr.mli
+++ plugins/qcheck-stm/src/reserr.mli
@@ -1,10 +1,10 @@
 module W = Ortac_core.Warnings
 
 type W.kind +=
-  | Value_is_a_constant of string
-  | Value_return_sut of string
-  | Value_have_no_sut_argument of string
-  | Value_have_multiple_sut_arguments of string
+  | Constant_value of string
+  | Returning_sut of string
+  | No_sut_argument of string
+  | Multiple_sut_arguments of string
 
 type 'a reserr
 
