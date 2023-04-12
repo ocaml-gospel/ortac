9f65f961420d08c6994e7e8161338dcee4ea4ff6
Nicolas Osborne <nicolas.osborne@tarides.com>
2023-04-12 11:17:37 +0200

    Enrich reserr monad with a list of non fatal warnings
    The monad is now the pair of what it was and a list of warnings.
    We also add a promote and a map functions to collect information from a
    list of data from which we can build a reserr. The level function is
    used in the promote process in order to decide whether the warning is
    fatal or not.
    
    Co-authored-by: Samuel Hym <samuel.hym@rustyne.lautre.net>

--- plugins/qcheck-stm/src/reserr.ml
+++ plugins/qcheck-stm/src/reserr.ml
@@ -13,21 +13,31 @@ let level kind =
       W.Warning
   | _ -> W.level kind
 
-type 'a reserr = ('a, W.t list) result
+type 'a reserr = ('a, W.t list) result * W.t list
 
-let ok = Result.ok
-let error e = Result.error [ e ]
-let ( let* ) = Result.bind
+let ok x = (Result.ok x, [])
+let error e = (Result.error [ e ], [])
+let warns ws = (Result.ok (), ws)
+let warn w = warns [ w ]
 
-let ( and* ) a b =
-  match (a, b) with
-  | Error e0, Error e1 -> Error (e0 @ e1)
-  | Error e, _ | _, Error e -> Error e
-  | Ok a, Ok b -> Ok (a, b)
+let ( let* ) x f =
+  match x with
+  | Ok v, warns1 ->
+      let res, warns2 = f v in
+      (res, warns1 @ warns2)
+  | (Error _, _) as x -> x
 
-open Fmt
+let ( and* ) (a, aw) (b, bw) =
+  let r =
+    match (a, b) with
+    | Error e0, Error e1 -> Error (e0 @ e1)
+    | Error e, _ | _, Error e -> Error e
+    | Ok a, Ok b -> Ok (a, b)
+  in
+  (r, aw @ bw)
 
 let pp_kind ppf kind =
+  let open Fmt in
   match kind with
   | Constant_value id -> pf ppf "%a is a constant." W.quoted id
   | Returning_sut id -> pf ppf "%a returns a sut." W.quoted id
@@ -36,5 +46,27 @@ let pp_kind ppf kind =
       pf ppf "%a have multiple sut arguments." W.quoted id
   | _ -> W.pp_kind ppf kind
 
-let pp_errors = W.pp_param pp_kind level |> list
-let pp pp_ok = Fmt.result ~ok:pp_ok ~error:pp_errors
+let pp_errors = W.pp_param pp_kind level |> Fmt.list
+let pp pp_ok = Fmt.(pair (result ~ok:pp_ok ~error:pp_errors) pp_errors)
+
+let promote r =
+  let rec aux = function
+    | [] -> ok []
+    | ((Ok _, _) as x) :: xs ->
+        let* y = x and* ys = aux xs in
+        ok (y :: ys)
+    | (Error errs, ws) :: xs ->
+        let* _ = warns ws and* _ = auxes errs in
+        aux xs
+  and auxes = function
+    | [] -> ok ()
+    | ((k, _) as e) :: es -> (
+        match level k with
+        | W.Warning ->
+            let* _ = warn e in
+            auxes es
+        | W.Error -> error e)
+  in
+  aux r
+
+let map f l = List.map f l |> promote
--- plugins/qcheck-stm/src/reserr.mli
+++ plugins/qcheck-stm/src/reserr.mli
@@ -10,6 +10,10 @@ type 'a reserr
 
 val ok : 'a -> 'a reserr
 val error : W.t -> 'a reserr
+val warns : W.t list -> unit reserr
+val warn : W.t -> unit reserr
 val ( let* ) : 'a reserr -> ('a -> 'b reserr) -> 'b reserr
 val ( and* ) : 'a reserr -> 'b reserr -> ('a * 'b) reserr
+val promote : 'a reserr list -> 'a list reserr
+val map : ('a -> 'b reserr) -> 'a list -> 'b list reserr
 val pp : 'a Fmt.t -> 'a reserr Fmt.t
