d5a7478a62e9bf41aa7f5a36fb4a2a9e2a486336
Nicolas Osborne <nicolas.osborne@tarides.com>
2023-04-18 18:49:35 +0200

    Type translation ocaml_of_gospel
    The two added functions are necessary to generate the `state` type in
    the qcheck-stm plugin, but their logic is independant of this use case.
    As pure translation from gospel to OCaml, they are expose in
    `Ortac_core`.

--- src/core/ocaml_of_gospel.mli
+++ src/core/ocaml_of_gospel.mli
@@ -2,5 +2,14 @@ val pattern : Gospel.Tterm.pattern_node -> Ppxlib.pattern
 (** [pattern pn] translates a Gospel pattern into the matching OCaml pattern *)
 
 val term : context:Context.t -> Gospel.Tterm.term -> Ppxlib.expression
-(** [term ~context t] translated a Gospel typed term into the corresponding
+(** [term ~context t] translates a Gospel typed term into the corresponding
     OCaml value using the translation [context] *)
+
+val core_type_of_ty_with_subst :
+  (string -> Ppxlib.core_type option) -> Gospel.Ttypes.ty -> Ppxlib.core_type
+(** [core_type_of_ty_with_subst subst ty] translates a Gospel type into the
+    corresponding OCaml [core_type] applying [subst] on the type variables *)
+
+val core_type_of_tysymbol : Gospel.Ttypes.tysymbol -> Ppxlib.core_type
+(** [core_type_of_tysymbol ts] translates a Gospel type symbol into the
+    corresponding OCaml [core_type] **)
