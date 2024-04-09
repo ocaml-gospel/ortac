2d691a4a8ffc95b5dab50fc0b54e13c8c4f7c66a
Nicolas Osborne <nicolas.osborne@tarides.com>
2024-04-09 11:58:00 +0200

    Define a default to handle the absence of command on the commandline
    Correctly handle `--help` and `--version` without any command thanks to
    the new default
    Improve the error message when no command is given, with a different
    error message whether at least one plugin is available or not
    Remove the custom `usage` function, rely on the default Cmdliner usage
    message when it is helpful
    
    Co-authored-by: Samuel Hym <samuel.hym@rustyne.lautre.net>

--- bin/cli.ml
+++ bin/cli.ml
@@ -5,25 +5,31 @@ let () =
 
 open Cmdliner
 
-let usage () =
-  Format.(fprintf err_formatter)
-    "@[ortac: required plugin is missing, please install at least one@ \
-     (qcheck-stm,@ monolith@ or@ wrapper).@]@.";
-  exit Cmd.Exit.cli_error
+let default_cmd cmds =
+  let errmsg, usage =
+    match cmds with
+    | [] ->
+        (* We cannot be in the case where ORTAC_ONLY_PLUGIN was set, since
+           trying to load only one missing plugin would have raised an exception
+           earlier, so we know no plugin is available *)
+        (* Displaying usage in that case is useless *)
+        ("error: no plugin is available, please install at least one", false)
+    | _ -> ("error: missing command", true)
+  in
+  Term.(term_result ~usage (const (Result.error (`Msg errmsg))))
 
 let () =
-  match
+  let doc = "Run ORTAC."
+  and version =
+    Printf.sprintf "ortac version: %s"
+      (match Build_info.V1.version () with
+      | None -> "n/a"
+      | Some v -> Build_info.V1.Version.to_string v)
+  in
+  let info = Cmd.info "ortac" ~doc ~version
+  and cmds =
     Registration.fold (fun acc cmd -> cmd :: acc) [] Registration.plugins
-  with
-  | [] -> usage ()
-  | cmds ->
-      let doc = "Run ORTAC." in
-      let version =
-        Printf.sprintf "ortac version: %s"
-          (match Build_info.V1.version () with
-          | None -> "n/a"
-          | Some v -> Build_info.V1.Version.to_string v)
-      in
-      let info = Cmd.info "ortac" ~doc ~version in
-      let group = Cmd.group info cmds in
-      Stdlib.exit (Cmd.eval group)
+  in
+  let default = default_cmd cmds in
+  let group = Cmd.group info ~default cmds in
+  Stdlib.exit (Cmd.eval group)
