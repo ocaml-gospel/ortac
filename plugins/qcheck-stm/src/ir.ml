065f2f707bc41fb524d6b0684d02bb3e3a0d536e
Nicolas Osborne <nicolas.osborne@tarides.com>
2024-05-16 17:47:49 +0200

    Remove dead code

--- plugins/qcheck-stm/src/ir.ml
+++ plugins/qcheck-stm/src/ir.ml
@@ -85,27 +85,3 @@ type t = {
   ghost_functions : Tast.function_ list;
   values : value list;
 }
-
-let pp_state ppf state =
-  let open Fmt in
-  let pp_model ppf (id, ty) =
-    pf ppf "@[%a: %a@]" Ident.pp id Ppxlib_ast.Pprintast.core_type ty
-  in
-  pf ppf "@[%a@]@." (list ~sep:(any "; ") pp_model) state
-
-let pp_inst ppf inst =
-  let open Fmt in
-  let pp_binding ppf (v, t) =
-    pf ppf "%a/%a" string v Ppxlib_ast.Pprintast.core_type t
-  in
-  pf ppf "[%a]" (list ~sep:(any ", ") pp_binding) inst
-
-let pp_value ppf v =
-  let open Fmt in
-  pf ppf "id = %a; ty = %a; inst = %a@." Ident.pp v.id
-    Ppxlib_ast.Pprintast.core_type v.ty pp_inst v.inst
-
-let pp ppf t =
-  let open Fmt in
-  pf ppf "@[state = %a@]@[values = %a@]@." pp_state t.state (list pp_value)
-    t.values
