77631b7f453b2d00ec474be0767b0fd6974b06d8
Nikolaus Huber <mail@n-huber.eu>
2024-09-03 10:31:29 +0200

    Update invariant test
    - Use sequence instead of list (as apparently Gospel doesn't offer an append function for lists?)
    - Include test with multiple suts

--- plugins/qcheck-stm/test/invariants.mli
+++ plugins/qcheck-stm/test/invariants.mli
@@ -1,13 +1,20 @@
 type 'a t
-(*@ mutable model contents : 'a list
+(*@ mutable model contents : 'a sequence
     with x
-    invariant List.length x.contents > 0 *)
+    invariant Sequence.length x.contents > 0 *)
 
 val create : 'a -> 'a t
 (*@ t = create a
-    ensures t.contents = a :: [] *)
+    ensures t.contents = Sequence.singleton a *)
 
 val push : 'a -> 'a t -> unit
 (*@ push a t
     modifies t
-    ensures t.contents = a :: (old t.contents) *)
+    ensures t.contents = Sequence.cons a (old t.contents) *)
+
+val transfer : 'a t -> 'a t -> unit
+(*@ transfer t1 t2
+    modifies t1
+    modifies t2
+    ensures t1.contents = Sequence.singleton (Sequence.hd (old t1.contents))
+    ensures t2.contents = Sequence.tl (old t1.contents) ++ (old t2.contents) *)
