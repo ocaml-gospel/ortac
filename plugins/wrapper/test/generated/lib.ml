45487f1dcd7b8b4d59d41bafb1241af4cc5d1d25
Lucccyo <cha.git@mailo.fr>
2025-04-08 09:55:57 +0200

    error found in lib.ml

--- plugins/wrapper/test/generated/lib.ml
+++ plugins/wrapper/test/generated/lib.ml
@@ -2,4 +2,4 @@ type set = { size : int; mutable mask : int }
 
 let create n = { size = n; mask = 0 }
 let add i s = s.mask <- s.mask lor (1 lsl i)
-let mem i s = s.mask lor (1 lsl i) <> 0
+let mem i s = s.mask land (1 lsl i) <> 0
