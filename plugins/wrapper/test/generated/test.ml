d26867c5d7c8499d84506fbe6c4f2e9f847c6403
Lucccyo <cha.git@mailo.fr>
2025-04-08 16:01:41 +0200

    add test cases on Wrapper

--- plugins/wrapper/test/generated/test.ml
+++ plugins/wrapper/test/generated/test.ml
@@ -1,25 +1,52 @@
-module To_test = struct
-  let create = Lib.create
-  let add = Lib.add
-  let mem = Lib.mem
-end
+module To_test = Wrapper
 
+(* create *)
 let test_create () =
   let s = To_test.create 5 in
-  Alcotest.(check int) "size should be 5" 5 s.size;
-  Alcotest.(check int) "initial mask" 0 s.mask
+  Alcotest.(check int)  "size should be 5" 5 s.size;
+  Alcotest.(check int)  "initial mask" 0 s.mask;
+  Alcotest.(check bool) "3 should not be in set" false (To_test.mem 3 s)
 
-let test_add_mem () =
-  let s = To_test.create 8 in
+let test_neg_create () =
+  try
+    let _ = To_test.create (-1) in
+    Alcotest.fail "Negativ size"
+  with Ortac_runtime.Error _ -> ()
+
+(* add *)
+let test_add () =
+  let s = To_test.create 5 in
   To_test.add 2 s;
   Alcotest.(check bool) "2 should be in set" true (To_test.mem 2 s);
   Alcotest.(check bool) "3 should not be in set" false (To_test.mem 3 s)
 
+let test_neg_add () =
+  let s = To_test.create 5 in
+  try
+    let _ = To_test.add (-1) s in
+    Alcotest.fail "Negativ argument"
+  with Ortac_runtime.Error _ -> ()
+
+(* mem *)
+let test_neg_mem () =
+  let s = To_test.create 5 in
+  try
+    let _ = To_test.mem (-1) s in
+    Alcotest.fail "Negativ argument"
+  with Ortac_runtime.Error _ -> ()
+
 let () =
   let open Alcotest in
-  run "Expected" [
-    "lib.set", [
-      test_case "Create set"     `Quick test_create;
-      test_case "Add and mem"    `Quick test_add_mem;
+  run "Wrapped lib" [
+    "create", [
+      test_case "create is correct."              `Quick test_create;
+      test_case "create fails with negativ size." `Quick test_neg_create;
+    ];
+    "add", [
+      test_case "add is correct."             `Quick test_add;
+      test_case "add fails with negativ arg." `Quick test_neg_add;
+    ];
+    "mem", [
+      test_case "mem fails with negativ arg"  `Quick test_neg_mem;
     ];
   ]
