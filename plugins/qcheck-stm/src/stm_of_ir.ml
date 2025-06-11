7d9ecb33ff0a70554798579ed5d9475fa91a62c1
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-06-11 16:51:00 +0200

    Make ortac qcheck-stm generate STM_domain
    This first version makes the following design choice: when testing in a
    concurrent context, we don't store the new SUTs created during the
    tests.
    
    This weakens the tests of the sequential prefix compared to the
    sequential tests.
    
    The rational is about ease of implementation, allowing for tesing with
    domains without having to differentiate between commands ran in the
    sequential prefix and those ran in one of the spawned domain.

--- plugins/qcheck-stm/src/stm_of_ir.ml
+++ plugins/qcheck-stm/src/stm_of_ir.ml
@@ -482,7 +482,8 @@ let run_case config sut_name value =
       List.fold_right aux suts body
     in
     let pushes =
-      if Cfg.does_return_sut config value.ty then
+      (* If concurrent, don't push *)
+      if Cfg.((not config.domain) && does_return_sut config value.ty) then
         (* We can only push a sut if there was no exception *)
         if may_raise_exception value then
           [
@@ -541,7 +542,9 @@ let next_state_case state config state_ident nb_models value =
     else
       let vbs, sut_map = pop_states state_ident value in
       let sut_map =
-        if Cfg.does_return_sut config value.ty then
+        (* Don't create a name for a new sut when testing in concurrent
+           context *)
+        if Cfg.((*not config.domain &&*) does_return_sut config value.ty) then
           let ret_id = List.hd value.ret in
           let ret_var = gen_symbol ~prefix:(str_of_ident ret_id) () in
           (ret_id, Ident.create ~loc:Location.none ret_var) :: sut_map
@@ -631,7 +634,8 @@ let next_state_case state config state_ident nb_models value =
           next_state_vars
       in
       let push_expr =
-        if Cfg.does_return_sut config value.ty then
+        (* If concurrent, don't push *)
+        if Cfg.((not config.domain) && does_return_sut config value.ty) then
           let ret_id = List.hd value.ret in
           let ret_var = List.assoc ret_id next_vars_assoc in
           eapply (qualify [ "Model" ] "push") [ push_expr; evar ret_var ]
@@ -1616,16 +1620,19 @@ let pp_ortac_cmd_case config suts last value =
     let lhs =
       let pat = pvar "lhs" in
       let expr =
+        let ok_name =
+          if Cfg.(config.domain) then estring "Ok _"
+          else [%expr "Ok " ^ SUT.get_name [%e evar suts] 0]
+        in
         pexp_ifthenelse (evar last) (estring "r")
           (Some
              (if
                 Cfg.does_return_sut config value.ty && may_raise_exception value
-              then
-                res_match
-                  [%expr "Ok " ^ SUT.get_name [%e evar suts] 0]
-                  (estring "_")
-              else if Cfg.does_return_sut config value.ty then
-                eapply (qualify [ "SUT" ] "get_name") [ evar suts; eint 0 ]
+              then res_match ok_name (estring "_")
+                (* If concurrent, we wouldn't have pushed a new SUT *)
+              else if
+                Cfg.((not config.domain) && does_return_sut config value.ty)
+              then eapply (qualify [ "SUT" ] "get_name") [ evar suts; eint 0 ]
               else estring "_"))
       in
       value_binding ~pat ~expr
@@ -1633,8 +1640,10 @@ let pp_ortac_cmd_case config suts last value =
     let shift =
       let pat = pvar "shift" in
       let expr =
-        if Cfg.does_return_sut config value.ty && may_raise_exception value then
-          res_match (eint 1) (eint 0)
+        (* If concurrent, we wouldn't have pushed a new SUT *)
+        if Cfg.(config.domain) then eint 0
+        else if Cfg.does_return_sut config value.ty && may_raise_exception value
+        then res_match (eint 1) (eint 0)
         else if Cfg.does_return_sut config value.ty then eint 1
         else eint 0
       in
@@ -1771,11 +1780,13 @@ let stm config ir =
     let m, ms = split_list @@ prefix @ (module_name :: submodule) in
     noloc @@ List.fold_left (fun acc x -> Ldot (acc, x)) (Lident m) ms
   in
+  let ortac_runtime =
+    if config.domain then
+      [%stri module Ortac_runtime = Ortac_runtime_qcheck_stm_domain]
+    else [%stri module Ortac_runtime = Ortac_runtime_qcheck_stm_sequential]
+  in
   ok
-    (warn
-     :: open_mod opened_mod
-     :: [%stri module Ortac_runtime = Ortac_runtime_qcheck_stm_sequential]
-     :: ghost_types
+    ((warn :: open_mod opened_mod :: ortac_runtime :: ghost_types)
     @ ghost_functions
     @ sut_mod
     @ model_mod
