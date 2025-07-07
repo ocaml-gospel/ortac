59a83440796dfa8e2a782431a5b5209a414bada4
Charlène_Gros <78226952+Lucccyo@users.noreply.github.com>
2025-07-07 11:24:41 +0200

    Revert "Remove Ortac/Monolith plugin temporarily"

---
+++ plugins/monolith/src/ortac_monolith.mli
@@ -0,0 +1,4 @@
+val generate : string -> Format.formatter -> unit
+(** [generate path output] generate the code of the tests corresponding to the
+    specifications present in [path] in the monolith configuration and print it
+    on the [output] channel *)
