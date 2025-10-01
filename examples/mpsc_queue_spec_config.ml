074539d3e07cd91e169bc8736a35d35f5112df65
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-10-01 15:55:11 +0200

    [examples] Add frequencies to mpsc queue config
    So that `dom0` is the only consumer.

--- examples/mpsc_queue_spec_config.ml
+++ examples/mpsc_queue_spec_config.ml
@@ -6,3 +6,16 @@ module Gen = struct
   let int = nat
   let list = small_list
 end
+
+module Frequencies_dom1 = struct
+  let create = 0
+  let of_list = 0
+  let is_empty = 0
+  let close = 0
+  let pop_exn = 0
+  let pop_opt = 0
+  let drop_exn = 0
+  let peek_exn = 0
+  let peek_opt = 0
+  let push_head = 0
+end
--- examples/mpsc_queue_spec_tests.ml
+++ examples/mpsc_queue_spec_tests.ml
@@ -239,7 +239,24 @@ module Spec =
                (1, ((pure (fun a_3 -> Push_head a_3)) <*> int))])
     let arb_cmd_seq = arb_cmd
     let arb_cmd_dom0 = arb_cmd
-    let arb_cmd_dom1 = arb_cmd
+    let arb_cmd_dom1 _ =
+      let open QCheck in
+        make ~print:show_cmd
+          (let open Gen in
+             frequency
+               [(0, ((pure (fun () -> Create ())) <*> unit));
+               (0,
+                 ((pure (fun xs -> Of_list xs)) <*> (list small_signed_int)));
+               (1, ((pure (fun a_2 -> Push a_2)) <*> int));
+               (1, ((pure (fun xs_1 -> Push_all xs_1)) <*> (list int)));
+               (0, (pure Is_empty));
+               (0, (pure Close));
+               (0, (pure Pop_exn));
+               (0, (pure Pop_opt));
+               (0, (pure Drop_exn));
+               (0, (pure Peek_exn));
+               (0, (pure Peek_opt));
+               (0, ((pure (fun a_3 -> Push_head a_3)) <*> int))])
     let next_state cmd__002_ state__003_ =
       match cmd__002_ with
       | Create () ->
